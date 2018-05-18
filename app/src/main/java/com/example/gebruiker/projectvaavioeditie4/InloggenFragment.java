d3c0a20641c1b809894ff36b2f6f41132bec6f5b
Yvanovits <39337112+Yvanovits@users.noreply.github.com>
2018-05-18 11:02:35 +0200

    Fragments Koppelen
    Het koppelen van het wachtwoord vergeten en registeren fragment

--- app/src/main/java/com/example/gebruiker/projectvaavioeditie4/InloggenFragment.java
+++ app/src/main/java/com/example/gebruiker/projectvaavioeditie4/InloggenFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class InloggenFragment extends Fragment {
 
@@ -14,6 +16,27 @@ public class InloggenFragment extends Fragment {
     @Override
     //Het koppelen van de layout aan et fragment. 'OnCreate', dus wanneer het fragment wordt gemaakt maakt hij gebruikt van de layout genoemnd in onderstaande code.
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_inloggen, container, false);
+        View view = inflater.inflate(R.layout.fragment_inloggen, container, false);
+
+        TextView TextViewRegistreren = (TextView) view.findViewById(R.id.TextViewRegistreren);
+        TextViewRegistreren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new RegistrerenFragment()).addToBackStack("tag").commit();
+            }
+        });
+
+        TextView TextViewWachtwoordVergeten = (TextView) view.findViewById(R.id.TextViewWachtwoordVergeten);
+        TextViewWachtwoordVergeten.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new WachtwoordVergetenFragment()).addToBackStack("tag").commit();
+            }
+        });
+        return view;
     }
+
 }
+
--- app/src/main/java/com/example/gebruiker/projectvaavioeditie4/WachtwoordVergetenFragment.java
+++ app/src/main/java/com/example/gebruiker/projectvaavioeditie4/WachtwoordVergetenFragment.java
@@ -11,26 +11,23 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class RegistrerenFragment extends Fragment {
+public class WachtwoordVergetenFragment extends Fragment {
 
     @Nullable
     @Override
     //Het koppelen van de layout aan et fragment. 'OnCreate', dus wanneer het fragment wordt gemaakt maakt hij gebruikt van de layout genoemnd in onderstaande code.
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_registreren, container, false);
+        View view = inflater.inflate(R.layout.fragment_wwvergeten, container, false);
 
-        Button RegistratieVoltooirenBtn = (Button) view.findViewById(R.id.RegistratieVoltooienBtn);
-        RegistratieVoltooirenBtn.setOnClickListener(new View.OnClickListener() {
+        Button HerstellenBtn = (Button) view.findViewById(R.id.HerstellenBtn);
+        HerstellenBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
                 fragmentTransaction.replace(R.id.fragment_container, new InloggenFragment()).commit();
-                Toast.makeText(getActivity(), "U account is aangemaakt, u kunt nu inloggen", Toast.LENGTH_LONG).show();
+                Toast.makeText(getActivity(), "Volg de intructies in de mail om uw wachtwoord te herstellen", Toast.LENGTH_LONG).show();
             }
         });
-
         return view;
     }
 }
-
-
