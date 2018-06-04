a8252f6321c1b086971328d6ecc1fd94aef46cd3
Yvanovits <39337112+Yvanovits@users.noreply.github.com>
2018-06-04 13:48:15 +0200

    Eerste pogingen met database inloggen

--- app/src/main/java/com/example/gebruiker/projectvaavioeditie4/InloggenFragment.java
+++ app/src/main/java/com/example/gebruiker/projectvaavioeditie4/InloggenFragment.java
@@ -5,17 +5,30 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class InloggenFragment extends Fragment {
 
+    private EditText mEmail;
+    private EditText mWachtwoord;
+    private Button mInlog;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
     @Nullable
     @Override
-    //Het koppelen van de layout aan et fragment. 'OnCreate', dus wanneer het fragment wordt gemaakt maakt hij gebruikt van de layout genoemnd in onderstaande code.
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_inloggen, container, false);
 
@@ -36,17 +49,61 @@ public class InloggenFragment extends Fragment {
                 fragmentTransaction.replace(R.id.fragment_container, new WachtwoordVergetenFragment()).addToBackStack("tag").commit();
             }
         });
-        Button InlogBtn = (Button) view.findViewById(R.id.InlogBtn);
+
+        mEmail = (EditText) view.findViewById(R.id.EditTextEmail);
+        mWachtwoord = (EditText) view.findViewById(R.id.EditTextWachtwoord);
+        mInlog = (Button) view.findViewById(R.id.InlogBtn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() != null){
+
+                    FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_container, new ProfielWerknemerFragment()).addToBackStack("tag").commit();
+
+                }
+            }
+        };
+
+        mInlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mEmail.getText().toString();
+                String wachtwoord = mWachtwoord.getText().toString();
+
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(wachtwoord)){
+                    Toast.makeText(getActivity(), "Velden zijn leeg", Toast.LENGTH_LONG).show();
+                } else {
+
+                    mAuth.signInWithEmailAndPassword(email, wachtwoord).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(getActivity(), "Probleem bij het inloggen", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+       /* Button InlogBtn = (Button) view.findViewById(R.id.InlogBtn);
         InlogBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
                 fragmentTransaction.replace(R.id.fragment_container, new ProfielWerknemerFragment()).addToBackStack("tag").commit();
             }
-        });
+        }); */
 
         return view;
-    }
 
+    }
 }
 
--- app/src/main/java/com/example/gebruiker/projectvaavioeditie4/RegistrerenFragment.java
+++ app/src/main/java/com/example/gebruiker/projectvaavioeditie4/RegistrerenFragment.java
@@ -6,15 +6,23 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class RegistrerenFragment extends Fragment {
+
+    private DatabaseReference mDatabase;
+    private EditText mGebruikersnaam;
+    private EditText mWachtwoord;
+    private EditText mEmail;
+    private EditText mTelefoonnummer;
+    private Button mRegistreren;
 
     @Nullable
     @Override
@@ -22,15 +30,6 @@ public class RegistrerenFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_registreren, container, false);
 
-        Button RegistratieVoltooirenBtn = (Button) view.findViewById(R.id.RegistratieVoltooienBtn);
-        RegistratieVoltooirenBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_container, new InloggenFragment()).commit();
-                Toast.makeText(getActivity(), "U account is aangemaakt, u kunt nu inloggen", Toast.LENGTH_LONG).show();
-            }
-        });
         TextView AlgemeneVoorwardenTextView = (TextView) view.findViewById(R.id.AlgemeneVoorwaardenTextView);
         AlgemeneVoorwardenTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +38,44 @@ public class RegistrerenFragment extends Fragment {
                 startActivity(av);
             }
         });
+
+        // mDatabase = FirebaseDatabase.getInstance().getReference("Users");
+
+        // mGebruikersnaam = (EditText) view.findViewById(R.id.EditTextGebruikersnaam);
+        mWachtwoord = (EditText) view.findViewById(R.id.EditTextWachtwoord);
+        mEmail = (EditText) view.findViewById(R.id.EditTextEmail);
+        // mTelefoonnummer = (EditText) view.findViewById(R.id.EditTextTelefoonnummer);
+        mRegistreren = (Button) view.findViewById(R.id.RegistratieVoltooienBtn);
+
+        /* mRegistreren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String gebruikersnaam = mGebruikersnaam.getText().toString().trim();
+                String wachtwoord = mWachtwoord.getText().toString().trim();
+                String email = mEmail.getText().toString().trim();
+                String telefoonnummer = mTelefoonnummer.getText().toString().trim();
+
+                HashMap<String, String> dataMap = new HashMap<String, String>();
+                dataMap.put("Gebruikersnaam", gebruikersnaam);
+                dataMap.put("Wachtwoord", wachtwoord);
+                dataMap.put("Ëmail", email);
+                dataMap.put("Telefoonnummer", telefoonnummer);
+
+                mDatabase.push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                            fragmentTransaction.replace(R.id.fragment_container, new InloggenFragment()).commit();
+                            Toast.makeText(getActivity(), "U account is aangemaakt, u kunt nu inloggen", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getActivity(), "Er is iets fout gegaan, probeer het opnieuw", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        }); */
 
         return view;
     }
